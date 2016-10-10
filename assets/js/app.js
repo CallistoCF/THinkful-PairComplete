26eec55505aa0291f751341167eee0d6189283ef
Chris Frater <chrisfrater@gmail.com>
2016-10-10 00:25:58 -0700

    Finally!
    There was an issue with overlay fading into background, just
    brute-forced Jquery to change stuff- overlays are nuts!  apparently
    overlays over overlays is an ongoing issue/was cause of the fading of
    the title

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,22 @@ $(function() {
     $('.toggle-nav').click(function() {
         if ($('body').hasClass('show-nav')) {
             $('body').removeClass('show-nav').addClass('hide-nav');
+            $(".overlay").css("z-index", "-5");
+            $(".lbox").css("z-index", "7");
+            $(".sbox").css("z-index", "7");
+
             setTimeout(function() {
                 $('body').removeClass('hide-nav');
+                $(".overlay").css("z-index", "-5");
+                $(".lbox").css("z-index", "7");
+                $(".sbox").css("z-index", "7");
             }, 500);
             $('.lineBreak').show();
         } else {
             $('body').removeClass('hide-nav').addClass('show-nav');
+            $(".overlay").css("z-index", "15");
+            $(".lbox").css("z-index", "0");
+            $(".sbox").css("z-index", "0");
             $('.lineBreak').hide();
         }
         return false;
